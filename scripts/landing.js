b2cd75e82fd43711715e0d4f6224ace9491ef4b3
samuelhany-cpu <57716420+samuelhany-cpu@users.noreply.github.com>
2024-05-04 00:54:43 +0300

    Add files via upload

---
+++ scripts/landing.js
@@ -0,0 +1,44 @@
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const num = document.querySelector('.num');
+
+let a = 1;
+
+plus.addEventListener('click', () => {
+    a++;
+    num.textContent = a;
+    });
+minus.addEventListener('click', () => {
+    if (a > 1) {
+        a--;
+        num.textContent = a;
+    }
+});
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
